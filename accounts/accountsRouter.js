8b22cb6883cb1b15102a82ac97f504df46eec830
Francis Bulus <notchera@gmail.com>
2019-11-04 14:06:43 +0100

    created endpoint for posting new account

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("../data/dbConfig");
 const router = express.Router();
 
+//Endpoint for getting all accounts
 router.get("/", (req, res) => {
   db("accounts")
     .then(result => {
@@ -45,10 +46,41 @@ router.delete("/:id", (req, res) => {
       }
     })
     .catch(err => {
+      res.status(500).json({
+        message: "We encountered an error while deleting the account"
+      });
+    });
+});
+
+//Endpointing for updating an account
+router.put("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(data => {
+      if (data) {
+        res.status(200).json({ message: "The account was updated" });
+      } else {
+        res.status(404).json({ message: "The account ID is invalid" });
+      }
+    })
+    .catch(err => {
       res
         .status(500)
-        .json({
-          message: "We encountered an error while deleting the account"
-        });
+        .json({ message: "We encountered an error updating the account" });
+    });
+});
+
+//Endpoint for posting new account
+router.post("/", (req, res) => {
+  db("accounts")
+    .insert(req.body)
+    .then(ids => {
+      res.status(201).json({ account: ids[0] });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "We encountered an error creating the account" });
     });
 });
